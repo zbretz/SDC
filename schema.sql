9ca494eff81c96894f828a6a5cce0a67fdebbbe1
Zach <zachary.bretz@gmail.com>
2021-08-12 13:12:56 -0700

    Building material for psql scripts: create table and query statements

--- schema.sql
+++ schema.sql
@@ -1,8 +1,42 @@
-create table question (product_id int not null,
+-- to run this file
+
+-- create table questions (
+-- id int not null,
+-- -- id int not null AUTOINCREMENT??
+-- -- id serial,
+-- product_id int not null,
+-- body text,
+-- date_written bigint,
+-- asker_name text,
+-- asker_email text,
+-- reported int,
+-- helpful int,
+-- primary key(id));
+
+-- copy questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+-- from '/users/zach/workspace/hackreactor/sdc/CSV/questions.csv'
+-- delimiter ','
+-- csv header;
+
+create table answers (
+id serial,
+-- id int not null AUTOINCREMENT??
+-- id serial,
+question_id int not null,
 body text,
-date_written int,
-asker_name text,
-asket_email text,
+date_written bigint,
+answerer_name text,
+answerer_email text,
 reported int,
 helpful int,
-primary key(product_id));
+primary key(id));
+
+copy answers
+from '/users/zach/workspace/hackreactor/sdc/CSV/answers.csv'
+delimiter ','
+csv header;
+
+
+
+-- select * from questions where (asker_name='jbilas');
+-- select * from answers where (answerer_name='marcanthony');
