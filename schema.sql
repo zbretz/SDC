97ad23d206f9df5dda29af466883118f25b57098
Zach <zachary.bretz@gmail.com>
2021-08-12 16:06:08 -0700

    Added more complex queries to later benchmark for speed/efficiency/etc

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
--- to run this file
+-- to run this file:
+-- psql -U postgres postgres  -f schema.sql
 
 -- create table questions (
 -- id int not null,
@@ -11,32 +12,73 @@
 -- asker_email text,
 -- reported int,
 -- helpful int,
--- primary key(id));
+-- primary key(id)
+-- );
 
 -- copy questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
 -- from '/users/zach/workspace/hackreactor/sdc/CSV/questions.csv'
 -- delimiter ','
 -- csv header;
 
-create table answers (
-id serial,
--- id int not null AUTOINCREMENT??
+-- create table answers (
 -- id serial,
-question_id int not null,
-body text,
-date_written bigint,
-answerer_name text,
-answerer_email text,
-reported int,
-helpful int,
-primary key(id));
+-- -- id int not null AUTOINCREMENT??
+-- -- id serial,
+-- question_id int not null,
+-- body text,
+-- date_written bigint,
+-- answerer_name text,
+-- answerer_email text,
+-- reported int,
+-- helpful int,
+-- primary key(id)
+-- );
 
-copy answers
-from '/users/zach/workspace/hackreactor/sdc/CSV/answers.csv'
-delimiter ','
-csv header;
+-- copy answers
+-- from '/users/zach/workspace/hackreactor/sdc/CSV/answers.csv'
+-- delimiter ','
+-- csv header;
 
+-- create table answers_photos (
+-- id serial,
+-- answer_id int,
+-- url text,
+-- primary key(id)
+-- );
 
+-- copy answers_photos
+-- from '/users/zach/workspace/hackreactor/sdc/CSV/answers_photos.csv'
+-- delimiter ','
+-- csv header;
 
 -- select * from questions where (asker_name='jbilas');
 -- select * from answers where (answerer_name='marcanthony');
+
+--Building Query: GET /qa/questions
+-- select * from questions where (product_id='5');
+-- select * from answers where (question_id='34');
+
+-- select a.id, a.question_id from answers a inner join questions q on a.question_id = q.id limit 10;
+
+-- select q.id, a.question_id from answers a inner join questions q on a.question_id = q.id where product_id='66';
+
+
+-- select count(id) from answers;
+
+
+
+-- select id from questions  where product_id='66';
+-- --result: 214, 214, 216
+
+-- select id from answers where (question_id='214')
+-- -- result: 421, 422
+-- select id from answers where (question_id='215')
+-- -- 423, 424
+-- select id from answers where (question_id='216')
+-- -- 425, 426, 427, 428
+
+
+-- select q.id, q.body, a.question_id, a.body  from answers a inner join questions q on a.question_id = q.id where product_id='66';
+
+-- select id from answers where question_id in (select id from questions where product_id='66');
+
