968ef19b76a574d5f1334f93841c877791816a80
Zach <zachary.bretz@gmail.com>
2021-08-13 14:19:54 -0700

    Fleshed out server/db files with some boilerplate code

---
+++ express/db/index.js
@@ -0,0 +1,10 @@
+// boilerplate code from:
+// https://node-postgres.com/guides/project-structure
+
+// const { Pool } = require('pg')
+// const pool = new Pool()
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback)
+//   },
+// }
