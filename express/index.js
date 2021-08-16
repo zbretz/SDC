3ddf2e75e4f98e5739897cff852cf5b07b8ea9bc
Zach <zachary.bretz@gmail.com>
2021-08-16 11:05:28 -0700

    Reimplemented 'question' query with promises.

--- express/index.js
+++ express/index.js
@@ -10,14 +10,30 @@ app.get('/', (req, res) => {
 // Questions list
 app.get('/api/qa/questions', (req, res) => {
   const product_id = req.query['product_id']
-  db.query('SELECT * FROM questions WHERE product_id = $1 LIMIT 100',[product_id], (err, result) => {
-    if (err) {
-      return next(err)
-    }
-    res.send(result.rows[0])
-    // pool.end()
-  })
+  db.query('SELECT * FROM questions WHERE product_id = $1 LIMIT 100',[product_id])
+    .then(result => res.send(result.rows[0]))
+    .catch(e => console.error(e.stack))
 })
+
+// "results": [{
+//   "question_id": 37,
+//   "question_body": "Why is this product cheaper here than other sites?",
+//   "question_date": "2018-10-18T00:00:00.000Z",
+//   "asker_name": "williamsmith",
+//   "question_helpfulness": 4,
+//   "reported": false,
+//   "answers": {
+//     68: {
+//       "id": 68,
+//       "body": "We are selling it here without any markup from the middleman!",
+//       "date": "2018-08-18T00:00:00.000Z",
+//       "answerer_name": "Seller",
+//       "helpfulness": 4,
+//       "photos": []
+//       // ...
+//     }
+//   }
+// },
 
 // Answer list
 app.get('/api/qa/questions/:question_id/answers', (req, res) => {
@@ -35,7 +51,10 @@ app.get('/api/qa/questions/:question_id/answers', (req, res) => {
 
 // Add a Question
 app.post('/api/qa/questions', (req, res) => {
-
+  // const {body, name, email, product_id} = req.query
+  const values = ['body', 'name', 'email', 'product_id'].map(k => req.query[k]);
+  const text = 'INSERT INTO questions (body, name, email, product_id) VALUES($1, $2) RETURNING *'
+   res.send(values)
 })
 
 // Add an Answer
